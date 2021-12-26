b72ff6982e5549eee844d9beadb90e3eae6b41a9
Thanakorn kasemphisit <thanakorn.top1994@gmail.com>
2021-12-26 14:44:21 +0700

    delete page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { Route, Switch, Redirect } from "react-router-dom";
 import MainPage from "./components/pages/MainPage";
-import NewsPage from "./components/pages/NewsPage";
 
 function App() {
   return (
     <Switch>
       <Route path="/" exact>
         <Redirect to="/home" />
-      </Route>
-      <Route path="/news">
-        <NewsPage />
       </Route>
       <Route path="/home">
         <MainPage />
